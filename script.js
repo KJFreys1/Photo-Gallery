a6cb3a5304008b6669a79793741a81684df2a38a
KJFreys1 <kfreyermuth20@gmail.com>
2020-08-12 16:57:47 -0700

    header

---
+++ script.js
@@ -0,0 +1,31 @@
+(function () {
+    const $change = $(".change")
+    const spans = ["Beautful", "Art", "Life", "Wonderful", "Amazing"]
+    let index = 0
+    
+    function animateSpan() {
+        let hide = {
+            opacity: 0,
+            marginLeft: "60px",
+        }
+        let show = {
+            opacity: 1,
+            marginLeft: "20px"
+        }
+        $change.animate(hide, 1000, () => {
+            $change.css("marginLeft", "0")
+        })
+        setTimeout(() => $change.text(spans[index]), 1000)
+        if (index < 4) { 
+            index++
+        } else {
+            index = 0
+        }
+        $change.animate(show, 1000)
+    }
+    
+    setTimeout(() => {
+        animateSpan()
+        setInterval(animateSpan, 4000)
+    }, 2000)
+}())
