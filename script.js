0afbf85556e28db113ea03d56397e9f06d5c3624
KJFreys1 <kfreyermuth20@gmail.com>
2020-08-12 20:59:03 -0700

    destructured script file and added scroll transition

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 (function () {
     const $change = $(".change")
+    const styles = {
+        hide: {
+            opacity: 0,
+            marginLeft: "60px",
+        }, show: {
+            opacity: 1,
+            marginLeft: "20px"
+        }
+    }
     const spans = ["Beautful", "Art", "Life", "Complex", "Amazing"]
     let index = 0
     
     function animateSpan() {
-        let hide = {
-            opacity: 0,
-            marginLeft: "60px",
-        }
-        let show = {
-            opacity: 1,
-            marginLeft: "20px"
-        }
-        $change.animate(hide, 1000, () => {
+        $change.animate(styles.hide, 1000, () => {
             $change.css("marginLeft", "0")
             $change.text(spans[index])
         })
         index = index < 4 ? index + 1 : 0
-        $change.animate(show, 1000)
+        $change.animate(styles.show, 1000)
     }
     
     setTimeout(() => {
         animateSpan()
         setInterval(animateSpan, 4000)
     }, 2000)
+
+    // Scroll transition behavior
+    $(".nav a").on("click", function(e) {
+        if (this.hash) {
+            e.preventDefault()
+            $("html, body").animate({
+                scrollTop: $(this.hash).offset().top
+            }, 800)
+        }
+    })
 }())
